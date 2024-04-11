32860ed3b725ed8f334a50def5472d6ccaf28d50
Aquon Bovell <aquon_bovell@outlook.com>
2024-04-11 15:41:36 -0400

    added asteroids

--- AsteroidFragment.glsl
+++ AsteroidFragment.glsl
@@ -1,6 +1,11 @@
 #version 120
 
+uniform sampler2D asteroidTexture;
+
+varying vec2 UV;
+varying vec3 Position;
+
 void main()
     {
-        gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+        gl_FragColor = texture2D( asteroidTexture, UV );
     }
